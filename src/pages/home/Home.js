63921b1f056ddc01f1bd76e480ca2a6e9936753e
fadly <fadlystwn@gmail.com>
2021-08-17 12:20:25 +0700

    change passing props data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Home.css'
 import Book from '../../components/book/Book';
+import {Books} from '../../api-data';
 
 const Home = () => {
     return (
         <div className="container">
            <h1>Promo buy 2 get 1!</h1>
            <hr/>
-           <Book/>
+            <Book books={Books}/>
         </div>
     )
 }
